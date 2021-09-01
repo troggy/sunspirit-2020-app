0e9be7eea9b59f000d90fb5024a5d67a0afc6d86
Kosta Korenkov <kosta@korenkov.net>
2021-09-01 13:53:47 +0300

    fix: new public link format for spreadsheet

--- src/lib/spreadsheet.ts
+++ src/lib/spreadsheet.ts
@@ -6,7 +6,7 @@ const Tabletop = require("tabletop");
 export const readSpreadsheet = async (sheetName: string): Promise<Artist[]> => {
   return Tabletop.init({
     key:
-      "https://docs.google.com/spreadsheets/d/1fGLxOfXsMIIbz0KKjM6bj9ChBb1dMQTY9KxiVDOR8JI/edit?usp=sharing"
+      "https://docs.google.com/spreadsheets/d/1fGLxOfXsMIIbz0KKjM6bj9ChBb1dMQTY9KxiVDOR8JI/pubhtml"
   }).then(function (data: any) {
     return data[sheetName].all() as Artist[];
   });
